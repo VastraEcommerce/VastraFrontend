274a4683eaff7cf0301082635af69e1358c9ee90
Leeefo <a.m.abouelleef@gmail.com>
2022-07-06 14:06:16 +0200

    creating pages folder and basic routing guides

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
 function App() {
-  return <div className="btn rounded-none">Hello</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

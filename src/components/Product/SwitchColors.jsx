7f94bedc5691e4c1def05c07f4058e3ce5e816ae
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-07-07 01:22:35 +0200

    Fix color button width

--- src/components/Product/SwitchColors.jsx
+++ src/components/Product/SwitchColors.jsx
@@ -1,6 +1,5 @@
 export default function SwitchColors({
   colors = [],
-  size = 8,
   onChangeColorStateHandler = (colorValue = '') => {},
   currentColorState = '',
 }) {
@@ -12,7 +11,7 @@ export default function SwitchColors({
           name="color"
           value={color}
           onClick={() => onChangeColorStateHandler(color)}
-          className={`radio w-${size} h-${size} p-0 min-h-0 rounded-full shadow-sm shadow-black ring-black ring-offset-1 hover:shadow hover:ring-1 hover:scale-105 hover:ring-black active:ring-2 transition-all ease-in-out duration-150${
+          className={`radio p-0 min-h-0 rounded-full shadow-sm shadow-black ring-black ring-offset-1 hover:shadow hover:ring-1 hover:scale-105 hover:ring-black active:ring-2 transition-all ease-in-out duration-150${
             color === currentColorState
               ? 'shadow ring-2 hover:ring-2 scale-105 '
               : ''

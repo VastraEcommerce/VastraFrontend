85a137d642a80b1bd308f3a4088a34cf5993aaa0
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-07-07 14:43:47 +0200

    Make SwitchSizes component generic style

--- src/components/Product/SwitchSizes.jsx
+++ src/components/Product/SwitchSizes.jsx
@@ -3,15 +3,20 @@ export default function SwitchSizes({
   sizes = [],
   onChangeSizeStateHandler = (sizeValue = '') => {},
   currentSizeState = '',
+  boxSize = 9,
+  className = '',
 }) {
   return (
-    <div className="flex flex-wrap justify-center lg:justify-start items-center gap-5 py-2 px-1">
+    <div
+      className={`flex flex-wrap justify-center lg:justify-start items-center gap-1 ${className}`}
+    >
       {sizes.map((size) => (
         <button
           key={size}
-          className="btn btn-outline rounded-none font-normal min-h-0 h-[auto] p-2.5 border border-gray-300 text-gray-600"
+          className={`btn btn-xs btn-outline rounded-none font-normal min-h-0 h-[auto] p-2 border border-gray-300 text-gray-600`}
           onClick={() => onChangeSizeStateHandler(size)}
           style={{
+            padding: boxSize,
             outline:
               size === currentSizeState
                 ? 'solid 1.65px black'

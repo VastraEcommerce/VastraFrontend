ac0968c55c38722031b88d5d2094aa100c492b05
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-07-07 14:41:53 +0200

    Add options for rate stars to be readonly

--- src/components/Product/RateStars.jsx
+++ src/components/Product/RateStars.jsx
@@ -1,9 +1,15 @@
 import themes from 'daisyui/src/colors/themes';
 import { FaStarHalfAlt, FaStar, FaRegStar } from 'react-icons/fa';
 import ReactStars from 'react-rating-stars-component';
-export default function RateStars({ size, value, onChange }) {
+export default function RateStars({
+  size,
+  value = 3.5,
+  onChange,
+  readOnly = false,
+}) {
   return (
     <ReactStars
+      edit={readOnly}
       count={5}
       size={size}
       filledIcon={<FaStar />}

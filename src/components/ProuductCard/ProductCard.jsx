ff65d250a735ce593a972e4fed14dca16382b7e8
Leeefo <a.m.abouelleef@gmail.com>
2022-07-22 02:28:16 +0200

    Modifying product card to display data from api

--- src/components/ProuductCard/ProductCard.jsx
+++ src/components/ProuductCard/ProductCard.jsx
@@ -1,70 +1,26 @@
-import { useState } from 'react';
-import { BiShoppingBag } from 'react-icons/bi';
-import { BsSuitHeart } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
-import RatStars from '../Product/RateStars';
-import SwitchColors from '../Product/SwitchColors';
-import SwitchSizes from '../Product/SwitchSizes';
-import img from './PlaceholderImage/Capture.PNG';
+import { useState } from "react";
+import { BiShoppingBag } from "react-icons/bi";
+import { BsSuitHeart } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import RatStars from "../Product/RateStars";
+import SwitchColors from "../Product/SwitchColors";
+import SwitchSizes from "../Product/SwitchSizes";
+import img from "./PlaceholderImage/Capture.PNG";
 
-const colors = ['#999966', ' #E6B333', '#9900B3', '#CC80CC', '#991AFF'];
-const sizes = ['xs', 's', 'sm', 'xl', 'xxl'];
-export default function ProductCard() {
+export default function ProductCard({ productInfo }) {
+  const variants = productInfo.variants.filter(
+    (varient) => varient.sizes.length > 0
+  );
+  const colors = variants.map((variant) => variant.color);
+  // const sizes = ["xs", "s", "sm", "xl", "xxl"];
+
   const [color, setColor] = useState(colors[0]);
+
+  const sizes = variants
+    .find((variant) => variant.color === color)
+    .sizes.map((size) => (size ? size.size : 0));
+
   const [size, setSize] = useState(sizes[0]);
-  // const product = {
-  //   brand: 'Deon_Halvorson98',
-  //   title: 'April.Runolfsdottir',
-  //   category: 'T-shirts',
-  //   variants: [
-  //     {
-  //       size: 'S',
-  //       price: 42,
-  //       color: ' #E6B333',
-  //       count: 54,
-  //     },
-  //     {
-  //       size: 'XXL',
-  //       price: 62,
-  //       color: ' #999966',
-  //       count: 77,
-  //     },
-  //     {
-  //       size: 'L',
-  //       price: 10,
-  //       color: '#FFB399',
-  //       count: 67,
-  //     },
-  //     {
-  //       size: 'L',
-  //       price: 11,
-  //       color: '#991AFF',
-  //       count: 49,
-  //     },
-  //     {
-  //       size: 'XXL',
-  //       price: 26,
-  //       color: ' #3366E6',
-  //       count: 31,
-  //     },
-  //     {
-  //       size: 'XL',
-  //       price: 44,
-  //       color: ' #9900B3',
-  //       count: 76,
-  //     },
-  //     {
-  //       size: 'S',
-  //       price: 64,
-  //       color: '#CC80CC ',
-  //       count: 47,
-  //     },
-  //   ],
-  //   description: 'Ulices71',
-  //   ratingsAverage: 1,
-  //   brand_thumbnail: 'Milan.Collier69',
-  //   ratingsQuantity: 16,
-  // };
 
   return (
     <div className="container py-5 border border-rose-600">
@@ -76,8 +32,13 @@ export default function ProductCard() {
         />
         <div className="ml-1">
           <div className="space-y-2">
-            <h3 className="text-xs text-light pt-2">Dolce & Gabbana</h3>
-            <p className="text-xs text-neutral-500">Jersey Graphic Tee Dolce</p>
+            <h3 className="text-xs text-light pt-2">
+              {productInfo.brand || "Dolce & Gabbana"}
+            </h3>
+            <p className="text-xs text-neutral-500">
+              {" "}
+              {productInfo.title || "Jersey Graphic Tee Dolce"}
+            </p>
           </div>
           <p className="space-x-2 mt-1">
             <span className="text-xl">{size}</span>
@@ -87,7 +48,7 @@ export default function ProductCard() {
               colors={colors}
               onChangeColorStateHandler={setColor}
               currentColorState={color}
-              circleSize={'1.2rem'}
+              circleSize={"1.2rem"}
             />
           </div>
           <div className="productSize flex justify-between w-[100%] text-center mb-4">
@@ -95,7 +56,7 @@ export default function ProductCard() {
               sizes={sizes}
               onChangeSizeStateHandler={setSize}
               currentSizeState={size}
-              boxSize={'0.4rem'}
+              boxSize={"0.4rem"}
             />
           </div>
           <div className="flex justify-start items-center pt-3 pb-2  mt-1">
@@ -118,7 +79,12 @@ export default function ProductCard() {
             </Link>
           </div>
           <div className=" my-3">
-            <RatStars size={15} value={5} onChange={''} readOnly={true} />
+            <RatStars
+              size={15}
+              value={productInfo.ratingsAverage}
+              onChange={""}
+              readOnly={true}
+            />
           </div>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
-import ProductCard from '../components/ProuductCard/ProductCard'
-const Home = () => {
+import ProductCard from "../components/ProuductCard/ProductCard";
+import { useGetAllProductsQuery } from "../features/services/products";
 
-  return <div>
-    <ProductCard />
-    </div>;
+const Home = () => {
+  const { data, error, isLoading } = useGetAllProductsQuery();
+
+  if (data) {
+    const productInfo = data.data[0];
+    console.log(productInfo);
+  }
+
+  return (
+    <div>
+      {data ? <ProductCard productInfo={data.data[0]} /> : <>No Data</>}
+    </div>
+  );
 };
 
 export default Home;

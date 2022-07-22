aec7b2ca09aa3cb386998c59af8125a629c9d4fe
Leeefo <a.m.abouelleef@gmail.com>
2022-07-22 04:39:36 +0200

    Adding hero section in the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Navbar from './components/Navbar';
-import ProductDetails from './pages/ProductDetails';
-import Footer from './components/Footer';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+import ProductDetails from "./pages/ProductDetails";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar>
-        <div className="container">
-          <Routes>
-            <Route path="/">
-              <Route index element={<Home />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/about" element={<About />} />
-              <Route path="products">
-                <Route index element={<div>Products</div>} />
-                <Route path=":productId" element={<ProductDetails />} />
-              </Route>
+        {/* <div className="container"> */}
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+            <Route path="products">
+              <Route index element={<div>Products</div>} />
+              <Route path=":productId" element={<ProductDetails />} />
             </Route>
-          </Routes>
-        </div>
+          </Route>
+        </Routes>
+        {/* </div> */}
         <Footer />
       </Navbar>
     </>

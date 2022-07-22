4a68eedf2393c1bfa90da42b1b831cdefc596df7
Leeefo <a.m.abouelleef@gmail.com>
2022-07-22 02:26:50 +0200

    Adding prodcut service to redux store

--- src/features/store.js
+++ src/features/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./product/productSlice";
+import { vastraApi } from "./services/products";
 
 export const store = configureStore({
     reducer: {
         product: productReducer,
+        [vastraApi.reducerPath]: vastraApi.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(vastraApi.middleware),
 });

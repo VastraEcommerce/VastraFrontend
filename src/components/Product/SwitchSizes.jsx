db292d76a2d4c9c43e682195acebf220399eaa75
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-07-30 06:14:29 +0200

    Refactoer switchSizes component

--- src/components/Product/SwitchSizes.jsx
+++ src/components/Product/SwitchSizes.jsx
@@ -1,19 +1,17 @@
 import themes from 'daisyui/src/colors/themes';
+
 export default function SwitchSizes({
-  sizes = [{ size: '', count: 0, price: 0 }],
-  onChangeSizeStateHandler = (
-    sizeValue = { size: '', count: 0, price: 0 }
-  ) => {},
-  currentSizeState = { size: '', count: 0, price: 0 },
+  sizes,
+  onChangeSizeStateHandler,
+  currentSizeState,
   boxSize = 9,
-  className = '',
+  className,
 }) {
-  console.log({ sizes, currentSizeState });
   return (
     <div
       className={`flex flex-wrap justify-center lg:justify-start items-center gap-1 ${className}`}
     >
-      {sizes.map((z) => (
+      {sizes?.map((z) => (
         <button
           key={z.size}
           className={`btn btn-xs btn-outline rounded-none font-normal min-h-0 h-[auto] p-2 border border-gray-300 text-gray-600`}
@@ -22,7 +20,7 @@ export default function SwitchSizes({
             padding: boxSize,
             minWidth: `${boxSize + 20}px`,
             outline:
-              z.size === currentSizeState.size
+              z.size === currentSizeState?.size
                 ? 'solid 1.65px black'
                 : themes['[data-theme=light]']['neutral-100'],
           }}

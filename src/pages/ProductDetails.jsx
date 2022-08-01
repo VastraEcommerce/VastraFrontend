30709641aec5ab2d589023d6e71f3d89e360663b
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-08-01 15:42:28 +0200

    Fix Initital state for varient

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useMemo, useState } from 'react';
-import { useParams, useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import { useGetProductByIdQuery } from '../services/productApi';
 
 import ImagesSlider from '../components/Product/ImagesSlider';
-import SwitchSizes from '../components/Product/SwitchSizes';
-import SwitchColors from '../components/Product/SwitchColors';
 import RateStars from '../components/Product/RateStars';
+import SwitchColors from '../components/Product/SwitchColors';
+import SwitchSizes from '../components/Product/SwitchSizes';
 
 export default function ProductDetails() {
   const { pathname } = useLocation();
@@ -13,46 +13,41 @@ export default function ProductDetails() {
   const id = params.productId;
 
   const { data, isError, isLoading } = useGetProductByIdQuery(id);
-  const product = data?.data;
+  console.log({ data });
 
-  const [variant, setVariant] = useState(product?.variants[0]);
+  const [variant, setVariant] = useState(() => data?.variants[0]);
   const [size, setSize] = useState(variant?.sizes[0]);
   const [color, setColor] = useState(variant?.color);
 
   const colors = useMemo(
-    () => product?.variants?.map((variant) => variant.color),
-    [product]
+    () => data?.variants?.map((variant) => variant.color),
+    [data]
   );
 
-  // useEffect(() => {
-  //   if (product) {
-  //     console.log('effect Product', product);
-  //     setVariant(product.variants[0]);
-  //     setSize(variant?.sizes[0]);
-  //   }
-  // }, [product, product?.variants, variant?.sizes]);
+  useEffect(() => {
+    const variant = data?.variants?.find((variant) => variant?.color === color);
+    setVariant(variant);
+    setSize(variant?.sizes[0]);
+  }, [color, data?.variants]);
+
+  useEffect(() => {
+    setVariant(data?.variants[0]);
+    setColor(data?.variants[0]?.color);
+    setSize(data?.variants[0]?.color);
+  }, [data]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
-  useEffect(() => {
-    const variant = product?.variants?.find(
-      (variant) => variant?.color === color
-    );
-    setVariant(variant);
-    setSize(variant?.sizes[0]);
-  }, [color, product?.variants]);
-
   if (isError) return <div>There is error</div>;
   if (isLoading) return <div>Loading...</div>;
-  if (product) console.log({ product });
   console.log({ variant });
 
   return (
     <div className="container">
       <div className="capitalize text-center py-4 text-xs md:text-sm xl:text-start">
-        home / {product?.category} / {product?.brand}
+        home / {data?.category} / {data?.brand}
       </div>
       <div className="flex flex-wrap justify-between gap-6">
         <div className="flex-grow min-h-[600px] w-full lg:w-[50%] md:basis-[calc(50%-1.5rem)] ">
@@ -63,22 +58,22 @@ export default function ProductDetails() {
             <span className="badge-error text-white px-2">{'hot'}</span>
             <span className="badge-info text-white px-2">{'in stock'}</span>
           </div>
-          <h1 className="text-xl lg:text-2xl">{product?.title}</h1>
+          <h1 className="text-xl lg:text-2xl">{data?.title}</h1>
           <div className="border-black flex justify-center lg:justify-start gap-x-2">
             <RateStars size={25} readOnly />
             <span className="underline uppercase font-thin">
-              {`${product?.ratingsQuantity} reviews`}
+              {`${data?.ratingsQuantity} reviews`}
             </span>
           </div>
           <div>
             <span className="uppercase text-slate-500">Brand:</span>
-            <span className="capitalize">{product?.brand}</span>
+            <span className="capitalize">{data?.brand}</span>
           </div>
           <h1 className="text-2xl lg:text-3xl">${'171.00'}</h1>
           <h3>
             Hurry Only{' '}
-            <span className="text-red-600">{product?.variants.length}</span>{' '}
-            left in Stock
+            <span className="text-red-600">{data?.variants.length}</span> left
+            in Stock
           </h3>
           <progress className="progress w-full" value={70} max={100}></progress>
           <hr className="my-4" />

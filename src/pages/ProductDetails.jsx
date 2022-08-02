fa4d3543e354d2133d3f8b5eeb398e37a8ce6eda
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-08-02 02:32:26 +0200

    Add Sadow for product details

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -46,35 +46,40 @@ export default function ProductDetails() {
   return (
     <div className="container">
       <div className="capitalize text-center py-4 text-xs md:text-sm xl:text-start">
-        home / {data?.category} / {data?.brand}
+        home / {data.category} / {data.brand}
       </div>
-      <div className="flex flex-wrap justify-between gap-6">
+      <div className="flex flex-wrap justify-between gap-6 shadow-xl">
         <div className="flex-grow min-h-[600px] w-full lg:w-[50%] md:basis-[calc(50%-1.5rem)] ">
-          {variant?.images && <ImagesSlider images={variant?.images} />}
+          {variant && <ImagesSlider images={variant?.images} />}
         </div>
-        <div className=" flex-grow w-full md:basis-[calc(50%-1.5rem)] flex flex-col text-center lg:text-start gap-y-4">
+        <div className=" flex-grow w-full md:basis-[calc(50%-1.5rem)] flex flex-col text-center lg:text-start gap-y-4 p-4">
           <div className="flex gap-x-2 justify-center capitalize text-sm lg:justify-start">
             <span className="badge-error text-white px-2">{'hot'}</span>
             <span className="badge-info text-white px-2">{'in stock'}</span>
           </div>
-          <h1 className="text-xl lg:text-2xl">{data?.title}</h1>
+          <h1 className="text-xl lg:text-2xl">{data.title}</h1>
           <div className="border-black flex justify-center lg:justify-start gap-x-2">
             <RateStars size={25} readOnly />
-            <span className="underline uppercase font-thin">
-              {`${data?.ratingsQuantity} reviews`}
-            </span>
+            <a className="uppercase link" href="#reviews">
+              {`${data.ratingsQuantity} reviews`}
+            </a>
           </div>
           <div>
             <span className="uppercase text-slate-500">Brand:</span>
-            <span className="capitalize">{data?.brand}</span>
+            <span className="capitalize">{data.brand}</span>
           </div>
+          {/* // TODO Price */}
           <h1 className="text-2xl lg:text-3xl">${'171.00'}</h1>
           <h3>
-            Hurry Only{' '}
-            <span className="text-red-600">{data?.variants.length}</span> left
-            in Stock
+            Hurry! Only{' '}
+            <span className="text-red-600">{data.variants.length}</span> left in
+            Stock!
           </h3>
-          <progress className="progress w-full" value={70} max={100}></progress>
+          <progress
+            className="progress w-full"
+            value={data.variants?.length * 10}
+            max={100}
+          ></progress>
           <hr className="my-4" />
           <div>
             <h1 className="uppercase font-thin mb-3">colors:{color}</h1>
@@ -83,18 +88,20 @@ export default function ProductDetails() {
               onChangeColorStateHandler={setColor}
               currentColorState={color}
               circleSize={25}
-              className="px-2 py-1 gap-6"
+              className="px-2 py-1 gap-3"
             />
           </div>
           <div>
             <h1 className="uppercase font-thin mb-3">sizes:{size?.size}</h1>
-            <SwitchSizes
-              sizes={variant?.sizes}
-              onChangeSizeStateHandler={setSize}
-              currentSizeState={size}
-              boxSize={10}
-              className="py-2 px-1 gap-x-3"
-            />
+            {variant && (
+              <SwitchSizes
+                sizes={variant?.sizes}
+                onChangeSizeStateHandler={setSize}
+                currentSizeState={size}
+                boxSize={10}
+                className="py-2 px-1 gap-x-3"
+              />
+            )}
           </div>
         </div>
       </div>

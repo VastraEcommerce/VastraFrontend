986a64da671496bb899c322eac1cf88abbce3154
MohamedShehtaa <mmshehta92@gmail.com>
2022-08-03 00:56:33 +0200

    fix middleware bug

--- src/app/store.js
+++ src/app/store.js
@@ -4,22 +4,16 @@ import { productApi } from '../services/productApi';
 import { usersApi } from '../services/usersApi';
 import { registerReducer, loginReducer } from './../pages/Account';
 
-export const store = configureStore({
+export const store = configureStore( {
   reducer: {
-    [productApi.reducerPath]: productApi.reducer,
-    [usersApi.reducerPath]: usersApi.reducer,
+    [ productApi.reducerPath ]: productApi.reducer,
+    [ usersApi.reducerPath ]: usersApi.reducer,
     register: registerReducer,
     login: loginReducer,
   },
-<<<<<<< HEAD
-  middleware: ( getDefaultMiddleware ) => getDefaultMiddleware().concat( productApi.middleware ).concat( usersApi.middleware )
-
+  middleware: ( getDefaultMiddleware ) =>
+    getDefaultMiddleware().concat( productApi.middleware ).concat( usersApi.middleware ),
 } );
-=======
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(productApi.middleware),
-});
->>>>>>> 1de879a298969d41bde2e9820f15562e48218fc6
 
 // Required for refetchOnFocus/refetchOnReconnect behaviors
-setupListeners(store.dispatch);
+setupListeners( store.dispatch );

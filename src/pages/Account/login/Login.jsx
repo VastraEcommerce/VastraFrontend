2502a02d888bb3e4b04ac8c3d3617cde8f9e88ed
Hassan Sabry <hassan_sabry92@hotmail.com>
2022-08-05 15:21:16 +0200

    Update Login Page

--- src/pages/Account/login/Login.jsx
+++ src/pages/Account/login/Login.jsx
@@ -18,13 +18,12 @@ const loginSchema = Yup.object().shape({
 export default function Login() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [incorrectSubmit, setIncorrectSubmit] = useState("");
   const [forgetPassword, setForgetPassword] = useState({
     email: "",
   });
   const [messageForgetPass, setMessageForgerPass] = useState("");
-
-  const [incorrectSubmit, setIncorrectSubmit] = useState("");
-  const forgetPasswordApi = () => {
+  const onClickForgetPassword = () => {
     if (forgetPassword.email !== "" && forgetPassword.email !== "Required") {
       axios
         .post(
@@ -32,7 +31,6 @@ export default function Login() {
           forgetPassword
         )
         .then(({ data: { message } }) => {
-          console.log(message);
           setMessageForgerPass(message);
         })
         .catch(
@@ -46,7 +44,6 @@ export default function Login() {
         );
     } else {
       setForgetPassword({ email: "Required" });
-      console.log(forgetPassword);
     }
   };
 
@@ -201,7 +198,7 @@ export default function Login() {
           <div className="text-center">
             <button
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-3"
-              onClick={forgetPasswordApi}
+              onClick={onClickForgetPassword}
             >
               Forget Password
             </button>

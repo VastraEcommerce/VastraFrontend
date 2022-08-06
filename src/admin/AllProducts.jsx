78433272e5946607b5d00c494176acf9d4e0b044
Leeefo <a.m.abouelleef@gmail.com>
2022-08-06 14:41:53 +0200

    Creating All products tab

---
+++ src/admin/AllProducts.jsx
@@ -0,0 +1,196 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+
+function createData(name, calories, fat, carbs, protein, price) {
+  return {
+    name,
+    calories,
+    fat,
+    carbs,
+    protein,
+    price,
+    history: [
+      {
+        date: "2020-01-05",
+        customerId: "11091700",
+        amount: 3,
+      },
+      {
+        date: "2020-01-02",
+        customerId: "Anonymous",
+        amount: 1,
+      },
+    ],
+  };
+}
+
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <>
+      {/* Accordion Head */}
+      <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
+        {/* Accordion Button */}
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+
+        <TableCell component="th" scope="row">
+          {row.title}
+        </TableCell>
+
+        <TableCell align="right">{row.brand}</TableCell>
+        <TableCell align="right">{row.category}</TableCell>
+        <TableCell align="right">{row.ratingsAverage}</TableCell>
+      </TableRow>
+
+      {/* Accordion Body */}
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                Variants
+              </Typography>
+
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    {/* <TableCell>Color</TableCell> */}
+                    <TableCell>Size</TableCell>
+                    <TableCell align="right">Stock</TableCell>
+                    <TableCell align="right">Price ($)</TableCell>
+                  </TableRow>
+                </TableHead>
+
+                <TableBody>
+                  <TableRow>
+                    <TableCell colSpan={12} sx={{ p: 0 }}>
+                      {row.variants.map((variant) => (
+                        <Table
+                          sx={{
+                            width: "100%",
+                          }}
+                          size="small"
+                          key={variant.color}
+                        >
+                          <TableHead>
+                            <TableRow>
+                              <TableCell
+                                sx={{
+                                  backgroundColor: variant.color,
+                                  width: "100px",
+                                }}
+                              ></TableCell>
+                              <TableCell>{variant.color}</TableCell>
+                            </TableRow>
+                          </TableHead>
+
+                          <TableBody>
+                            {variant.sizes.map((size) => (
+                              <TableRow key={size.size}>
+                                <TableCell>{size.size}</TableCell>
+                                <TableCell align="right">
+                                  {size.count}
+                                </TableCell>
+                                <TableCell align="right">
+                                  {size.price}
+                                </TableCell>
+                              </TableRow>
+                            ))}
+                          </TableBody>
+                        </Table>
+                      ))}
+                    </TableCell>
+                  </TableRow>
+                  {/* {variant.sizes.map((size) => (
+                        <TableRow key={variant.color}>
+                          <Box key={size.size}>
+                            <TableCell>{variant.color}</TableCell>
+                            <TableCell>{size.size}</TableCell>
+                            <TableCell align="right">{size.count}</TableCell>
+                            <TableCell align="right">{size.price}</TableCell>
+                          </Box>
+                        </TableRow>
+                      ))} */}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </>
+  );
+}
+
+// Row.propTypes = {
+//   row: PropTypes.shape({
+//     calories: PropTypes.number.isRequired,
+//     carbs: PropTypes.number.isRequired,
+//     fat: PropTypes.number.isRequired,
+//     history: PropTypes.arrayOf(
+//       PropTypes.shape({
+//         amount: PropTypes.number.isRequired,
+//         customerId: PropTypes.string.isRequired,
+//         date: PropTypes.string.isRequired,
+//       })
+//     ).isRequired,
+//     name: PropTypes.string.isRequired,
+//     price: PropTypes.number.isRequired,
+//     protein: PropTypes.number.isRequired,
+//   }).isRequired,
+// };
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0, 3.99),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3, 4.99),
+  createData("Eclair", 262, 16.0, 24, 6.0, 3.79),
+  createData("Cupcake", 305, 3.7, 67, 4.3, 2.5),
+  createData("Gingerbread", 356, 16.0, 49, 3.9, 1.5),
+];
+
+export default function AllProducts({ products }) {
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell>Title</TableCell>
+            <TableCell align="right">Brand</TableCell>
+            <TableCell align="right">Categories</TableCell>
+            <TableCell align="right">Rating</TableCell>
+          </TableRow>
+        </TableHead>
+
+        <TableBody>
+          {console.log(products)}
+          {products.map((row, index) => (
+            <Row key={row._id} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}

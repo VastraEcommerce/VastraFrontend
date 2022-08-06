f322fcf95e5afb5dbc34b517a1eaf919fff65493
Leeefo <a.m.abouelleef@gmail.com>
2022-08-06 14:41:21 +0200

    Creating product page

--- src/admin/pages/Products.jsx
+++ src/admin/pages/Products.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { useState } from "react";
+
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+
+import AddProduct from "../AddProduct";
+import AllProducts from "../AllProducts";
+import products from "../products.json";
 
 const Products = () => {
-  return <div>Products</div>;
+  const [value, setValue] = useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  // console.log(products);
+  return (
+    <>
+      <Box sx={{ width: "100%", typography: "body1" }}>
+        <TabContext value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <TabList onChange={handleChange} aria-label="lab API tabs example">
+              <Tab label="All products" value="1" />
+              <Tab label="Add Product" value="2" />
+              <Tab label="Item Three" value="3" />
+            </TabList>
+          </Box>
+          <TabPanel value="1">
+            <AllProducts products={products} />
+          </TabPanel>
+          <TabPanel value="2">
+            <AddProduct />
+          </TabPanel>
+          <TabPanel value="3">Item Three</TabPanel>
+        </TabContext>
+      </Box>
+    </>
+  );
 };
 
 export default Products;

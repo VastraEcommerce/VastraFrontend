9547fd6807ca3b61b9077593c1bda7a2eaeac8f7
MohamedShehtaa <mmshehta92@gmail.com>
2022-08-10 03:18:54 +0200

    continue for shopping bag and create the product in the page

--- src/components/shopingBag/ProductVeiw.jsx
+++ src/components/shopingBag/ProductVeiw.jsx
@@ -1,20 +1,28 @@
 import React from "react";
-
+// import { useSelector } from "react-redux";
+// import { useGetUserByIdQuery } from "../../services/userApi";
 import Product from "./Product";
+// import EmptyBag from "./Product";
 
 export default function ProductVeiw() {
+  // const user = useSelector((state) => state.auth.user);
+  // const { data, isLoading, isSuccess, isError } = useGetUserByIdQuery(user._id);
   return (
-    <div className='py-8 px-5'>
-      <table className='table'>
+    <div className='py-8 sm:px-5'>
+      <table className='table text-neutral table-normal'>
         <thead>
           <tr>
-            <th>PRODUCT</th>
-            <th>PRICE</th>
-            <th>QUANTIY</th>
-            <th>TOTAL</th>
+            <td className='text-sm font-normal'>PRODUCT</td>
+            <td className='text-sm font-normal'>PRICE</td>
+            <td className='text-sm font-normal'>QUANTIY</td>
+            <td className='text-sm font-normal'>TOTAL</td>
           </tr>
         </thead>
         <tbody>
+          <Product />
+          <Product />
+          <Product />
+          <Product />
           <Product />
         </tbody>
       </table>

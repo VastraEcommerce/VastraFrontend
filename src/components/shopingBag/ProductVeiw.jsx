de57f0f3b37d95a5e5ea324c669b2b0c74cbe9c6
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-08-12 17:56:57 +0200

    Fix cartItems api service

--- src/components/shopingBag/ProductVeiw.jsx
+++ src/components/shopingBag/ProductVeiw.jsx
@@ -1,23 +1,22 @@
-import React from "react";
-
-import Product from "./Product";
+import React from 'react';
+import CartItem from './CartItem';
 
 export default function ProductVeiw({ data }) {
   return (
-    <div className='py-8 sm:px-5 w-[100%]'>
-      <table className='table text-neutral table-normal w-[100%] text-center'>
+    <div className="py-8 sm:px-5 w-[100%]">
+      <table className="table text-neutral table-normal w-[100%] text-center">
         <thead>
           <tr>
-            <td className='text-sm font-normal'>PRODUCT</td>
-            <td className='text-sm font-normal'>PRODUCT SIZE</td>
-            <td className='text-sm font-normal'>PRICE</td>
-            <td className='text-sm font-normal'>QUANTIY</td>
-            <td className='text-sm font-normal'>TOTAL</td>
+            <td className="text-sm font-normal">PRODUCT</td>
+            <td className="text-sm font-normal">PRODUCT SIZE</td>
+            <td className="text-sm font-normal">PRICE</td>
+            <td className="text-sm font-normal">QUANTIY</td>
+            <td className="text-sm font-normal">TOTAL</td>
           </tr>
         </thead>
         <tbody>
-          {data.map((product, index) => {
-            return <Product product={product} key={index} />;
+          {data.map((product) => {
+            return <CartItem product={product} key={product._id} />;
           })}
         </tbody>
       </table>

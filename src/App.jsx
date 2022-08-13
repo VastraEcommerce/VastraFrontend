433bb571681087b7b08295e2ede7fb4388629372
MohamedShehtaa <mmshehta92@gmail.com>
2022-08-13 02:18:59 +0200

    create pages contact ,aboutUs  and not found and modify on productCard change image with color

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,65 @@
-import About from './pages/About';
-import Login from './pages/Account/login/Login';
-import Register from './pages/Account/register/Register';
-import Address from './pages/Address';
-import Contact from './pages/Contact';
-import ShoppingBag from './pages/ShopingBag';
-import Home from './pages/Home';
-import ProductDetails from './pages/ProductDetails';
-import Profile from './pages/Profile';
-import { Route, Routes } from 'react-router-dom';
-import Layout from './components/Layout';
-import RequireAuth, { roles } from './features/auth/RequireAuth';
-import UserList from './test/UserList'; // for testing onley it will be removed
-import Welcome from './test/Welcome'; // for testing onley it will be removed
-import AdminMain from './admin/AdminMain';
-import AdminLogin from './admin/pages/AdminLogin';
-import Products from './admin/pages/Products';
-import Dashboard from './admin/pages/Dashboard';
-
+import About from "./pages/About";
+import Login from "./pages/Account/login/Login";
+import Register from "./pages/Account/register/Register";
+import Address from "./pages/Address";
+import Contact from "./pages/Contact";
+import ShoppingBag from "./pages/ShopingBag";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import Profile from "./pages/Profile";
+import { Route, Routes, Redirect } from "react-router-dom";
+import Layout from "./components/Layout";
+import RequireAuth, { roles } from "./features/auth/RequireAuth";
+import UserList from "./test/UserList"; // for testing onley it will be removed
+import Welcome from "./test/Welcome"; // for testing onley it will be removed
+import AdminMain from "./admin/AdminMain";
+import AdminLogin from "./admin/pages/AdminLogin";
+import Products from "./admin/pages/Products";
+import Dashboard from "./admin/pages/Dashboard";
+import NotFound from "./pages/NotFound";
 function App() {
   return (
     <Routes>
-      <Route path="/admin">
-        <Route path="login" element={<AdminLogin />} />
+      <Route path='/admin'>
+        <Route path='login' element={<AdminLogin />} />
         <Route element={<AdminMain />}>
           <Route index element={<Dashboard />} />
-          <Route path="products" element={<Products />} />
+          <Route path='products' element={<Products />} />
         </Route>
       </Route>
-      <Route path="/" element={<Layout />}>
+      <Route path='/' element={<Layout />}>
         {/* Puplic Routes */}
+
         <Route index element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Register />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/cart" element={<ShoppingBag />} />
-        <Route path="products">
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Register />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/cart' element={<ShoppingBag />} />
+        <Route path='products'>
           <Route index element={<div>Products</div>} />
-          <Route path=":productId" element={<ProductDetails />} />
+          <Route path=':productId' element={<ProductDetails />} />
         </Route>
-        <Route path="unauthorized" element={<div>unauthorized</div>} />
+        <Route path='unauthorized' element={<div>unauthorized</div>} />
 
         {/* Protected Routes */}
         <Route element={<RequireAuth allowedRoles={[roles.USER]} />}>
-          <Route path="profile">
+          <Route path='profile'>
             <Route index element={<Profile />} />
-            <Route path="address" element={<Address />} />
+            <Route path='address' element={<Address />} />
           </Route>
         </Route>
 
         <Route element={<RequireAuth allowedRoles={[roles.ADMIN]} />}>
-          <Route path="userslist" element={<UserList />} />
+          <Route path='userslist' element={<UserList />} />
         </Route>
 
         <Route
-          element={<RequireAuth allowedRoles={[roles.ADMIN, roles.USER]} />}
-        >
-          <Route path="welcome" element={<Welcome />} />
+          element={<RequireAuth allowedRoles={[roles.ADMIN, roles.USER]} />}>
+          <Route path='/' />
         </Route>
       </Route>
+      <Route path='*' element={<NotFound />} />
     </Routes>
   );
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,171 +1,68 @@
 import Accordion from "../components/Accordion";
 
-const About = () => {
-  return (
-    <div>
-      <Accordion
-        header={"First Accordion"}
-        body={` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
-            autem magnam dolores excepturi dolorem rem sint, error, voluptatibus
-            beatae aut placeat porro ad. Quam, quo exercitationem excepturi eius
-            veritatis omnis. Lorem ipsum, dolor sit amet consectetur adipisicing
-            elit. Quo sit alias mollitia dolor magnam dignissimos ab esse, at
-            non suscipit, nesciunt dolores distinctio illo porro voluptatibus
-            explicabo assumenda earum tenetur. Lorem ipsum dolor sit amet,
-            consectetur adipisicing elit. Atque architecto tenetur nostrum vero,
-            deleniti maxime nam officia sunt minima, nemo quae doloremque
-            doloribus perferendis necessitatibus.`}
-      />
-      <Accordion
-        header={"Second Accordion"}
-        body={` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
-            autem magnam dolores excepturi dolorem rem sint, error, voluptatibus
-            beatae aut placeat porro ad. Quam, quo exercitationem excepturi eius
-            veritatis omnis. Lorem ipsum, dolor sit amet consectetur adipisicing
-            elit. Quo sit alias mollitia dolor magnam dignissimos ab esse, at
-            non suscipit, nesciunt dolores distinctio illo porro voluptatibus
-            explicabo assumenda earum tenetur. Lorem ipsum dolor sit amet,
-            consectetur adipisicing elit. Atque architecto tenetur nostrum vero,
-            deleniti maxime nam officia sunt minima, nemo quae doloremque
-            doloribus perferendis necessitatibus.`}
-      />
-      <Accordion
-        header={"Third Accordion"}
-        body={` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
-            autem magnam dolores excepturi dolorem rem sint, error, voluptatibus
-            beatae aut placeat porro ad. Quam, quo exercitationem excepturi eius
-            veritatis omnis. Lorem ipsum, dolor sit amet consectetur adipisicing
-            elit. Quo sit alias mollitia dolor magnam dignissimos ab esse, at
-            non suscipit, nesciunt dolores distinctio illo porro voluptatibus
-            explicabo assumenda earum tenetur. Lorem ipsum dolor sit amet,
-            consectetur adipisicing elit. Atque architecto tenetur nostrum vero,
-            deleniti maxime nam officia sunt minima, nemo quae doloremque
-            doloribus perferendis necessitatibus.`}
-      />
+import React from "react";
+import image from "../images/vastra.jpeg";
 
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+export default function About() {
+  return (
+    <div className='mb-14'>
+      <h1 className=' my-10 text-center text-2xl font-bold text-neutral'>
+        {" "}
+        WELCOME TO VASTRA{" "}
+      </h1>
+      <p className='my-5 text-xl text-neutral px-5'> About us: </p>
+      <div className='flex justify-evenly'>
+        <div className='w-[50%]'>
+          <p>{` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
+            autem magnam dolores excepturi dolorem rem sint, error, voluptatibus
+            beatae aut placeat porro ad. Quam, quo exercitationem excepturi eius
+         `}</p>
+          <Accordion
+            expanded={true}
+            header={"First Accordion"}
+            body={` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
+            autem magnam dolores excepturi dolorem rem sint, error, voluptatibus
+            beatae aut placeat porro ad. Quam, quo exercitationem excepturi eius
+            veritatis omnis. Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. Quo sit alias mollitia dolor magnam dignissimos ab esse, at
+            non suscipit, nesciunt dolores distinctio illo porro voluptatibus
+            explicabo assumenda earum tenetur. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Atque architecto tenetur nostrum vero,
+            deleniti maxime nam officia sunt minima, nemo quae doloremque
+            doloribus perferendis necessitatibus.`}
+          />
+          <Accordion
+            header={"Second Accordion"}
+            body={` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
+            autem magnam dolores excepturi dolorem rem sint, error, voluptatibus
+            beatae aut placeat porro ad. Quam, quo exercitationem excepturi eius
+            veritatis omnis. Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. Quo sit alias mollitia dolor magnam dignissimos ab esse, at
+            non suscipit, nesciunt dolores distinctio illo porro voluptatibus
+            explicabo assumenda earum tenetur. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Atque architecto tenetur nostrum vero,
+            deleniti maxime nam officia sunt minima, nemo quae doloremque
+            doloribus perferendis necessitatibus.`}
+          />
+          <Accordion
+            header={"Third Accordion"}
+            body={` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
+            autem magnam dolores excepturi dolorem rem sint, error, voluptatibus
+            beatae aut placeat porro ad. Quam, quo exercitationem excepturi eius
+            veritatis omnis. Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. Quo sit alias mollitia dolor magnam dignissimos ab esse, at
+            non suscipit, nesciunt dolores distinctio illo porro voluptatibus
+            explicabo assumenda earum tenetur. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Atque architecto tenetur nostrum vero,
+            deleniti maxime nam officia sunt minima, nemo quae doloremque
+            doloribus perferendis necessitatibus.`}
+          />
+        </div>
+        <div className='w-[30%]'>
+          <img src={image} alt='' />
+        </div>
+      </div>
       <br />
     </div>
   );
-};
-
-export default About;
+}

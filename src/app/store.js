bedfcd457cf04464339e799391c0f0ed58dddee1
Hassan Sabry <hassan_sabry92@hotmail.com>
2022-08-13 09:32:06 +0200

    Add Order Slice To Store

--- src/app/store.js
+++ src/app/store.js
@@ -1,25 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { setupListeners } from '@reduxjs/toolkit/query';
-import authSlice from '../features/auth/authSlice';
-import { productApi } from '../services/productApi';
-// import { usersApi } from '../services/usersApi';
-import { cartSlice } from './features/cartSlice';
-import { apiSlice } from './api/apiSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import authSlice from "../features/auth/authSlice";
+import { productApi } from "../services/productApi";
+import { cartSlice } from "./features/cartSlice";
+import { orderSlice } from "./features/orderSlice";
+import { apiSlice } from "./api/apiSlice";
 
-export const store = configureStore( {
+export const store = configureStore({
   reducer: {
-    [ productApi.reducerPath ]: productApi.reducer,
-    [ apiSlice.reducerPath ]: apiSlice.reducer,
+    [productApi.reducerPath]: productApi.reducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authSlice.reducer,
     shoppingBag: cartSlice.reducer,
+    order: orderSlice.reducer,
 
     // register: registerReducer,
     // login: loginReducer,
   },
-  middleware: ( getDefaultMiddleware ) =>
-    getDefaultMiddleware().concat( productApi.middleware ),
-  devTools: process.env.NODE_ENV !== 'production',
-} );
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productApi.middleware),
+  devTools: process.env.NODE_ENV !== "production",
+});
 
 // Required for refetchOnFocus/refetchOnReconnect behaviors
-setupListeners( store.dispatch );
+setupListeners(store.dispatch);
